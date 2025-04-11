2b7fbecb988eb49fc24a26d0c41d27593bfcb3c6
Artyom Ulanchik <artyom.u@outlook.com>
2025-04-12 00:20:58 +0100

    version bump

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
 const rootElement = document.getElementById("root")!;
 
 ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 );
